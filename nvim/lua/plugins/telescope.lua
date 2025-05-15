43645325963bd01db3f45c40c112af903d7d845d
chetanjangir0 <chetanrjj2002@gmail.com>
2025-05-15 16:34:00 +0530

    nvim -> telescope ignored node_modules, now can search hidden files/ . files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,12 +1,17 @@
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
 
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+      -- vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>ff", function()
+        builtin.find_files({ hidden = true })
+      end, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
       require("telescope").setup({
         extensions = {
@@ -22,14 +27,15 @@ return {
               preview_cutoff = 0,
             },
           },
+          file_ignore_patterns = { "node_modules/" },
         },
       })
 
       require("telescope").load_extension("ui-select")
-    end
+    end,
   },
 
   {
     "nvim-telescope/telescope-ui-select.nvim",
-  }
+  },
 }
