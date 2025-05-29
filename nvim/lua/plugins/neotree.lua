cacec89894b54367ac3b0991f59f736b5904ac85
chetanjangir0 <chetanrjj2002@gmail.com>
2025-05-29 17:17:03 +0530

    changes

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,12 +1,17 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<leader>t', ':Neotree filesystem reveal right<CR>', {})
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			window = {
+				width = 25,
+			},
+		})
+		vim.keymap.set("n", "<leader>t", ":Neotree filesystem reveal right<CR>", {})
+	end,
 }
