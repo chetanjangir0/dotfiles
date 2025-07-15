8eb845b793de1389ab9cedcbe79da4b139141954
chetanjangir0 <chetanrjj2002@gmail.com>
2025-07-15 15:01:01 +0530

    some refactoring and now using 4 space indentation instead of 2

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,5 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.wrap = false
-vim.opt.scrolloff = 8
+require("set")
+require("remap")
 
 require("config.lazy")
 --syntax highlight for waybar config files
@@ -15,20 +9,3 @@ vim.filetype.add({
   },
 })
 
--- my remaps ---------------
-
---copy and paste
-vim.keymap.set("n", "<Leader>y", '"+y', { noremap = true })
-vim.keymap.set("v", "<Leader>y", '"+y', { noremap = true })
-vim.keymap.set("n", "<Leader>p", '"+p', { noremap = true })
-vim.keymap.set("v", "<Leader>p", '"+p', { noremap = true })
-
---move lines up or down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
---edit all occurance of the word
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
---go to the other end of paranthesis
-vim.keymap.set("n", "<A-m>", "%", { noremap = true, silent = true })
