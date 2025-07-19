4d9ec404b54dc8a07645e99a06c8fd7c6633aa4e
chetanjangir0 <chetanrjj2002@gmail.com>
2025-07-19 11:54:44 +0530

    fixed the none-ls eslint_d error on javascript files that are not even using eslint

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,20 +1,30 @@
 return {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-        "nvimtools/none-ls-extras.nvim",
-    },
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                -- null_ls.builtins.completion.spell,
-                -- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
-                null_ls.builtins.formatting.stylua,
+	config = function()
+		local has_eslint = function(utils)
+			return utils.root_has_file({
+				".eslintrc.js",
+				".eslintrc.cjs",
+				".eslintrc.json",
+				"eslint.config.js",
+				"eslint.config.cjs",
+				"eslint.config.mjs",
+			})
+		end
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- null_ls.builtins.completion.spell,
+				-- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+				null_ls.builtins.formatting.stylua,
 
-                null_ls.builtins.formatting.prettier,
-                require("none-ls.diagnostics.eslint_d"),
-            },
-        })
-    end,
+				null_ls.builtins.formatting.prettier,
+				require("none-ls.diagnostics.eslint_d").with{condition=has_eslint},
+			},
+		})
+	end,
 }
