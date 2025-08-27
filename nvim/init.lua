395e8ced5a20318d93c330a7724ee76816dc38bf
chetanjangir0 <chetanrjj2002@gmail.com>
2025-08-27 10:35:37 +0530

    refactored

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,3 @@
 require("set")
 require("remap")
-
 require("config.lazy")
---syntax highlight for waybar config files
-vim.filetype.add({
-  pattern = {
-    [".*/waybar/config"] = "jsonc",
-  },
-})
-
