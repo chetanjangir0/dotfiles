e1357b8304c896416b3fa79a5807f5d5cc393b3d
chetanjangir0 <chetanrjj2002@gmail.com>
2025-09-10 12:30:25 +0530

    markdown auto text wrapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,12 @@
 require("set")
 require("remap")
 require("config.lazy")
+
+
+-- markdown text wrapping 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.textwidth = 80
+  end,
+})
