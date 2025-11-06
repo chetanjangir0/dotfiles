bf0d2b68a42feb1b6995b9b14b0f5ad47802b4b3
Chetan Jangir <chetanrjj2002@gmail.com>
2025-11-06 11:42:10 +0530

    refactor and add some useful nvim settings

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -7,11 +7,15 @@ map("n", "<Leader>p", '"+p', { noremap = true })
 map("v", "<Leader>p", '"+p', { noremap = true })
 
 --move lines up or down
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "move selection down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "move selection up" })
 
 --edit all occurance of the word
 map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 --go to the other end of paranthesis
 map("n", "<A-m>", "%", { noremap = true, silent = true })
+
+-- better indenting in visual mode
+map("v", "<", "<gv", { desc = "indent left and reselect" })
+map("v", ">", ">gv", { desc = "indent right and reselect" })
--- .config/nvim/lua/set.lua
+++ .config/nvim/lua/set.lua
@@ -1,30 +1,37 @@
-vim.g.mapleader = " "
+vim.g.mapleader = " " -- set leader key to space
 vim.g.maplocalleader = "\\"
 vim.opt.winborder = "rounded"
 
+-- basic
+vim.opt.number = true -- line numbers
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 8 --  keep 10 lines above/below cursor
+vim.opt.sidescrolloff = 8 -- keep 8 columns left/right to cursor
+
+-- indentation
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 4 -- number of spaces used when using >>
+vim.opt.autoindent = true -- copy indent from current line
+vim.opt.smartindent = true -- smart autoindent (eg after {, :, etc)
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.wrap = false
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-
+-- file handeling
+vim.opt.swapfile = false
 vim.opt.undofile = true
 vim.opt.undodir = vim.fn.stdpath("state") .. "/undo"
+vim.opt.updatetime = 50 -- shows faster lsp diagnostics after you stop typing
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.updatetime = 50
+-- search
 vim.opt.ignorecase = true
-vim.opt.shiftwidth = 4 -- number of spaces used when using >>
-vim.opt.termguicolors = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false -- don't highlight search results
+vim.opt.incsearch = true -- show matches as you type
 
-vim.opt.smartindent = true
+-- visual settings
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes" -- reserve space on left to display lsp etc signs
+vim.opt.showmode = false -- don't show mode(visual, normal ..) in cmd line
+vim.opt.pumheight = 10 -- maximum items in completion popup menu
+
