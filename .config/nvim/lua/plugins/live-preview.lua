9e326099c7cd9a76219878f01a0af2e5ed271c4f
Chetan Jangir <chetanrjj2002@gmail.com>
2025-11-16 18:34:01 +0530

    fix(nvim): typst preview not working

--- .config/nvim/lua/plugins/live-preview.lua
+++ .config/nvim/lua/plugins/live-preview.lua
@@ -2,5 +2,14 @@ return {
 	"chomosuke/typst-preview.nvim",
 	lazy = false, -- or ft = 'typst'
 	version = "1.*",
-	opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+	opts = {
+		debug = true,
+		dependencies_bin = {
+      -- because this plugins downloads its own tinymist which won't work on nixos 
+      -- because the downloaded plugin will be dynamically linked
+      -- websocat is fine because it is statically linked
+			["tinymist"] = "tinymist", -- tells this plugin to use system's own tinymist
+			["websocat"] = nil,
+		},
+	}, -- lazy.nvim will implicitly calls `setup {}`
 }
