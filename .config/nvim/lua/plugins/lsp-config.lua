d2530ee0c6feabd8e595ac196dd2a96aad4c45ae
Chetan Jangir <chetanrjj2002@gmail.com>
2025-11-19 21:23:35 +0530

    removed the hover remap of lsp because the remap was the default key all along

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -5,7 +5,6 @@ return {
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
             local lsp_keymaps = function()
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
                 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
                 vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
                 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
