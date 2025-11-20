cb07decc33f5a868766b1dc938fbb2326e26f566
Chetan Jangir <chetanrjj2002@gmail.com>
2025-11-20 20:21:37 +0530

    refactor(nvim): telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,58 +1,49 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = { "nvim-lua/plenary.nvim" },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
 
-		config = function()
-			local builtin = require("telescope.builtin")
+    config = function()
+      local builtin = require("telescope.builtin")
 
-			-- vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>ff", function()
-				builtin.find_files({ hidden = true })
-			end, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      -- vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>ff", function()
+        builtin.find_files({ hidden = true }) -- show dotfiles
+      end, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
-			local actions = require("telescope.actions")
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
 
-				pickers = {
-					colorscheme = {
-						enable_preview = true,
-					},
-				},
+        pickers = {
+          colorscheme = {
+            enable_preview = true,
+          },
+        },
 
-				defaults = {
-					-- mappings = {
-					-- 	i = { -- Insert mode mappings
-					-- 		["<C-j>"] = actions.move_selection_next,
-					-- 		["<C-k>"] = actions.move_selection_previous,
-					-- 	},
-					-- 	n = { -- Normal mode mappings (optional)
-					-- 		["<C-j>"] = actions.move_selection_next,
-					-- 		["<C-k>"] = actions.move_selection_previous,
-					-- 	},
-					-- },
+        defaults = {
+          color_devicons = true,
+          path_displays = { "smart" },
           -- for files preview in side bar
-					layout_config = {
-						horizontal = {
-							preview_cutoff = 0,
-						},
-					},
-					file_ignore_patterns = { "node_modules/", ".git/" },
-				},
-			})
+          layout_config = {
+            horizontal = {
+              preview_cutoff = 0,
+            },
+          },
+          -- file_ignore_patterns = { "node_modules/", ".git/" },
+        },
+      })
 
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-	},
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
 }
