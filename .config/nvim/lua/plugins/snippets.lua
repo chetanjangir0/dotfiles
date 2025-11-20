f9b19a5dff534ce81afbe6b71ba0f8a82dddc5da
Chetan Jangir <chetanrjj2002@gmail.com>
2025-11-20 16:12:00 +0530

    fix: snippets work now

--- .config/nvim/lua/plugins/snippets.lua
+++ .config/nvim/lua/plugins/snippets.lua
@@ -3,5 +3,16 @@ return {
   config = function()
     require("luasnip").setup({ enable_autosnippets = true })
     require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/" })
+
+    local ls = require("luasnip")
+    vim.keymap.set({ "i", "s" }, "<C-e>", function()
+      ls.expand_or_jump(1)
+    end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-J>", function()
+      ls.jump(1)
+    end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-K>", function()
+      ls.jump(-1)
+    end, { silent = true })
   end,
 }
