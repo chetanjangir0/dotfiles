6de419db718f095c04d13538dc7b3593d5ac8314
Chetan Jangir <chetanrjj2002@gmail.com>
2026-03-01 13:28:59 +0530

    add pyright lsp for python

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,43 +1,44 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      vim.lsp.enable({
-        "nil_ls",
-        "lua_ls",
-        "ts_ls",
-        "tailwindcss",
-        "eslint",
-        "marksman",
-        "rust_analyzer",
-        "gopls",
-        "tinymist",
-        "svelte",
-        "svelteserver"
-      })
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.lsp.enable({
+				"nil_ls",
+				"lua_ls",
+				"ts_ls",
+				"tailwindcss",
+				"eslint",
+				"marksman",
+				"rust_analyzer",
+				"gopls",
+				"tinymist",
+				"svelte",
+				"svelteserver",
+				"pyright",
+			})
 
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {}) -- default is gra
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-      vim.keymap.set("n", "<leader>dm", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-      vim.keymap.set("n", "<leader>dl", require("telescope.builtin").diagnostics)
-      vim.keymap.set("n", "<leader>fr", require("telescope.builtin").lsp_references)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {}) -- default is gra
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+			vim.keymap.set("n", "<leader>dm", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+			vim.keymap.set("n", "<leader>dl", require("telescope.builtin").diagnostics)
+			vim.keymap.set("n", "<leader>fr", require("telescope.builtin").lsp_references)
 
-      -- Configure built-in diagnostic display
-      vim.diagnostic.config({
-        virtual_text = {
-          prefix = "●", -- Or any character you like
-        },
-        update_in_insert = true,
-        float = {
-          focusable = false,
-          style = "minimal",
-          border = "rounded",
-          source = "always",
-          header = "",
-          prefix = "",
-        },
-      })
-    end,
-  },
+			-- Configure built-in diagnostic display
+			vim.diagnostic.config({
+				virtual_text = {
+					prefix = "●", -- Or any character you like
+				},
+				update_in_insert = true,
+				float = {
+					focusable = false,
+					style = "minimal",
+					border = "rounded",
+					source = "always",
+					header = "",
+					prefix = "",
+				},
+			})
+		end,
+	},
 }
